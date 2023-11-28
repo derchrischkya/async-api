655ffdc257bc91d96d0cb88f0530add7c29b8dec
Christoph Richter <crichter@cocus.com>
2023-11-28 14:14:30 +0100

    added inital project

--- api/tests/minimal.sh
+++ api/tests/minimal.sh
@@ -1,3 +1,12 @@
+curl 127.0.0.1:3000/api/v1/ping
+
+if [ $? -eq 0 ]; then
+    echo "API available"
+else
+    echo "API non available"
+    exit 1
+fi
+
 curl 127.0.0.1:3000/api/v1/process/run
 
 if [ $? -eq 0 ]; then
@@ -6,3 +15,5 @@ else
     echo "API not received message"
     exit 1
 fi
+
+exit 0
