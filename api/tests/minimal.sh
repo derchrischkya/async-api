484a68a2670997753dda7dfde51640a424b276a7
Christoph Richter <crichter@cocus.com>
2023-11-28 19:46:04 +0100

    run api-server in background

--- api/tests/minimal.sh
+++ api/tests/minimal.sh
@@ -7,7 +7,7 @@ else
     exit 1
 fi
 
-curl --silent "127.0.0.1:3000/api/v1/process/run" > /dev/null
+curl --silent "127.0.0.1:3000/api/v1/process/run"
 if [ $? -eq 0 ]; then
     echo "API received message"
 else
